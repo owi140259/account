a6735f199e721ff48ebf1855e0293b4c0b8c7c77
ajrskelton <admin@OWI-317.openwindow.ac.za>
2016-09-22 16:17:48 +0200

    Add settings and layout. Skin with CSS.

--- web/index.php
+++ web/index.php
@@ -2,12 +2,12 @@
     // web/index.php
     
     require_once __DIR__.'/../vendor/autoload.php';
-    
     use Symfony\Component\HttpFoundation\Request;
-
+    use Symfony\Component\HttpFoundation\Response;
+    
     $app = new Silex\Application();
-    $app['debug'] = true;
-
+    
+    // Service provider registrations go here
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/views',
     ));
@@ -16,11 +16,19 @@
     $app->get('/', function(Request $request) use ($app) {
         return "<h1>Hello World</h1>";
     });
-
-    // Routes go here
+    
     $app->get('/greeting/{person}', function(Request $request, $person) use ($app) {
-        return $app['twig']->render('hello.twig', array('name' => $person));
+        return $app['twig']->render('hello.twig', array('name'=>$person));
     });
 
+    $app->get('/settings', function(Request $request) use ($app) {
+        return $app['twig']->render('settings.twig', array());
+    });
+
+    $app->get('/friends', function(Request $request) use ($app) {
+        return $app['twig']->render('friends.twig', array());
+    });
+
+    // Run the app
     $app->run();
 ?>
--- web/views/layout.php
+++ web/views/layout.php
@@ -2,11 +2,26 @@
 <html>
     <head>
         <title>Account</title>
-        <link rel="stylesheet" href="../style/global.css">
+        <link rel="stylesheet" href="/account/web/style/global.css">
     </head>
     <body>
-        <p>Welcome to your account</p>
-        {% block content %}
-        {% endblock %}
+        <div id="banner">
+            ACCOUNT
+        </div>
+        <header class="well">
+            <div class="loggedin">
+                <p>You are logged in as User</p>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="/account/web/settings">SETTINGS</a></li>
+                    <li><a href="/account/web/friends">FRIENDS</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main class="well">
+            {% block content %}
+            {% endblock %}
+        </main>
     </body>
 </html>
